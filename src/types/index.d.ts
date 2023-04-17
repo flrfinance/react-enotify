176b62ad93556a6077acba9b623d4ced5729c0e7
bannik <me@bannik.eu>
2023-04-17 19:55:21 +0200

    fix: Fixing typed components

--- src/types/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { NotificationProps, NotificationType } from "./notification";
-declare module "@flrfinance/react-enotify" {
-  export const useNotifications: () => {
-    addNotification: (notification: NotificationType) => void;
-    removeNotification: (id: string) => void;
-  };
-  export const NotificationsProvider: React.FC;
-  export const Notification: React.FC<NotificationProps>;
-}
