bf979565838530467d0452ab2e4af01974806c05
bannik <me@bannik.eu>
2023-04-17 16:59:22 +0200

    fixing tests and build

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import {
+  useNotifications,
+  NotificationsProvider,
+} from "./context/useNotifications";
+import Notification from "./components/Notification";
+export { useNotifications, NotificationsProvider, Notification };
